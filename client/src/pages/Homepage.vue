4ca931c8afc79a588ef8cf4384e59146102919ed
Riley Kong <kongriley@gmail.com>
2021-01-15 22:09:33 -0800

    FIx tailwind

--- client/src/pages/Homepage.vue
+++ client/src/pages/Homepage.vue
@@ -2,8 +2,8 @@
   <div>
     <h1 class="font-medium text-6xl text-center pt-24">AMAZING PPE THINGY</h1>
     <p class="text-center text-xl pt-6">A logistical hub to connect PPE donors with front-line workers during COVID-19</p>
-    <img src="../assets/mask.png">
-    <img src="../assets/nurses.png">
+    <img src="@/assets/mask.png">
+    <img src="@/assets/nurses.png">
     <div class="flex justify-center pt-8">
       <div class="inline-flex text-center bg-blue-600 rounded-lg py-3 px-4 text-white">
         Get started
